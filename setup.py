9f703b12806339c8e20e2e2f3df759763f1ca3f3
kapilsingh09 <karan404katyura@gmail.com>
2026-04-02 21:22:46 +0530

    setup.py file ready

--- setup.py
+++ setup.py
@@ -1,18 +1,36 @@
 from setuptools import find_packages, setup
 from typing import List
 
-def get_requriements(filepath) ->[str]:
+def get_requriements() ->List[str]:
     """
     This function will return the list of requirements
      mentioned in the file passed as argument
     """
+    requirement_list: List[str] = []
     try:
-        with open('requirements.txt') as f:
-            return f.read().splitlines()
+        with open('requirements.txt','r') as f:
+            #read lines from the file
+            lines = f.readlines()
+            for line in lines:
+
+                requirement = line.strip()
+
+                if requirement and requirement != '-e .':
+                    requirement_list.append(requirement)
+
+    except FileNotFoundError as e:
+        print("requirements.txt not found - ",e)
+
+
+    return requirement_list
+
+# print(get_requriements())
 
 setup(
-    name="networksecurity",
-    version="0.1.0",
+    name='NetworkSecutiry',
+    version='0.0.01',
+    author='Kapil',
+    author_email="karan404katyura@gamil.com",
     packages=find_packages(),
-
+    install_requires=get_requriements()
 )
